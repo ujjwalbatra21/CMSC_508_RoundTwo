4d2e5fcff33e7d15701347acbd091fe6b228e273
ujjwalbtra21 <ujjwalbatra21@gmail.com>
2020-04-30 18:20:52 -0400

    Added connection.php

---
+++ 508Project-PHP/connection.php
@@ -0,0 +1,21 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$servername = "localhost";
+$username = "project_13";
+$password = "V00774677";
+$database = "project_13";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+?>
+
+
+
