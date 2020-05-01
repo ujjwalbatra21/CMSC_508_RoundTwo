971d895c51625106ae194e07aff6fbc22802ffa9
ujjwalbtra21 <ujjwalbatra21@gmail.com>
2020-05-01 18:09:29 -0400

    Added list_products.php

---
+++ 508Project-PHP/list_products.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+<style>
+table, th, td {
+  border: 1px solid black;
+}
+</style>
+</head>
+<body>
+
+<?php
+
+require_once 'connection.php';
+
+$stmt = $conn->prepare("SELECT product_name, brand, product_condition, price FROM product");
+$stmt->execute();
+
+
+if($stmt->rowCount() > 0){
+    echo "<table>";
+    echo "<table style='border: solid 1px black;'>";
+    echo "<tr>";
+    echo "<tr><th>ID</th><th>Employee Name</th></tr>";
+    echo "<tr><th>Name</th><th>Brand</th><th>Condition</th><th>Price</th></tr>";
+    echo "</tr>";
+    
+    echo "<tbody>";
+    
+    while($row = $stmt->fetch()){
+        echo "<tr>";
+        echo "<td>" . $row['product_name'] . "</td>";
+        echo "<td>" . $row['brand'] . "</td>";
+        echo "<td>" . $row['product_condition'] . "</td>";
+        echo "<td>" . $row['product_price'] . "</td>";
+        //echo "<td>" . $row['emp_name'] . "</td>";
+        echo "</tr>";
+    }
+    
+    echo "</tbody>";
+    echo "</table>";
+}
+
+else{
+    echo "<h4> No Products in the database.</h4>";
+}
+
+?>
+
+</body>
+</html>
