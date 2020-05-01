d08cb8bd710e539ff752ccc0fa856f832ff5da8d
ujjwalbtra21 <ujjwalbatra21@gmail.com>
2020-05-01 18:10:56 -0400

    Updated index.php to include product list

--- 508Project-PHP/index.php
+++ 508Project-PHP/index.php
@@ -4,5 +4,6 @@
 <h2> Round Two Database </h2> <br /> <br />
 <ul>
 <li> <a href="list_employees.php"> List all Employees </a> </li>
+<li> <a href="list_products.php"> Products available </a> </li>
 </ul>
 </body>
