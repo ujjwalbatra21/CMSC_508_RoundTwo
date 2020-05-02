1e4d6ebfcae362569c83c5d9afc8b58113f75b53
ujjwalbtra21 <ujjwalbatra21@gmail.com>
2020-05-02 12:31:59 -0400

    updated session

--- 508Project-PHP/session.php
+++ 508Project-PHP/session.php
@@ -4,7 +4,7 @@
    
    $user_check = $_SESSION['login_user'];
    
-   $ses_sql = mysqli_query($db,"select emp_name from admin where emp_name = '$user_check' ");
+   $ses_sql = mysqli_query($db,"select emp_name from employee where emp_name = '$user_check' ");
    
    $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
    
