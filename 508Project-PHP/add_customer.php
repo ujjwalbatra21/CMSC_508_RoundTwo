0a89f7c3570fa5f108368adec727970afbf14114
ujjwalbtra21 <ujjwalbatra21@gmail.com>
2020-05-02 14:56:02 -0400

    final add_customer update

--- 508Project-PHP/add_customer.php
+++ 508Project-PHP/add_customer.php
@@ -1,7 +1,6 @@
 <?php
 require_once 'connection.php';
 
-
 echo "<h3> Add a Customer </h3><br />";
 echo "<form method='post' action='add_customer.php'>";
 echo "<table style='border: solid 1px black;'>";
@@ -12,37 +11,10 @@ echo "<tr><td>Address</td>";
 echo "<td><input type='text' name='customer_address' size='15'></td></tr>";
 echo "<tr><td>Phone Number</td>";
 echo "<td><input type='text' name='customer_phone' size='15'></td></tr>";
-
-
-// echo "<tr><td>Brand</td>";
-// echo "<td><input type='text' name='brand' size='15'></td></tr>";
-// echo "<tr><td>Condition</td>";
-// echo "<td><select name='condition'>";
-// echo "<option value='default' selected>Select Condition</option>";
-// echo "<option value='new'>New</option>";
-// echo "<option value='used'>Used</option>";
-// echo "</select></td></tr>";
-// echo "<tr><td>Price</td>";
-// echo "<td><input type='text' name='price' size='10'></td></tr>";
-// echo "<tr><td>Color</td>";
-// echo "<td><input type='text' name='colorway' size='10'></td></tr>";
-// echo "<tr><td>Type</td>";
-// echo "<td><input type='text' name='type' size='15'></td></tr>";
-// echo "<tr><td>Gender</td>";
-// echo "<td><select name='gender'>";
-// echo "<option value='default' selected>Select Gender</option>";
-// echo "<option value='male'>Male</option>";
-// echo "<option value='female'>Female</option>";
-// echo "<option value='unisex'>Unisex</option>";
-// echo "<option value='kids'>Kids</option>";
-// echo "</select></td></tr>";
-// echo "<tr><td> <br /> </td></tr>";
 echo "<tr><td><input type='submit' name='submit' value='Submit'></td></tr>";
 echo "</tbody>";
 echo "</table>";
 echo "</form>";
-
-
 
 if(isset($_POST['submit'])){
     
@@ -52,20 +24,10 @@ if(isset($_POST['submit'])){
     $stmt->bindValue(':customer_name', $_POST['customer_name']);
     $stmt->bindValue(':customer_address', $_POST['customer_address']);
     $stmt->bindValue(':customer_phone', $_POST['customer_phone']);
-
-    
-    
-//     if($_POST['shoeid'] == ' '){
-//         $stmt->bindValue(':shoe_id', NULL, PDO::PARAM_STR);
-//     }
-//     if($_POST['clothesid'] == ' '){
-//         $stmt->bindValue(':clothes_id', NULL, PDO::PARAM_STR);
-//     }
     
     $stmt->execute();
     
-    echo "<h4> Data Inserted successfully...!! </h4>";
-    
+    echo "<h4> Registered Successfully! </h4>";  
 }
 
 ?>
