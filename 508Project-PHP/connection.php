9b097b8936aadfe6fb976bc6f1f289f40fe30816
ujjwalbtra21 <ujjwalbatra21@gmail.com>
2020-05-02 12:35:22 -0400

    updated connection

--- 508Project-PHP/connection.php
+++ 508Project-PHP/connection.php
@@ -7,6 +7,7 @@ $servername = "localhost";
 $username = "project_13";
 $password = "V00774677";
 $database = "project_13";
+$db = mysqli_connect($servername, $username, $password, $database); 
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
