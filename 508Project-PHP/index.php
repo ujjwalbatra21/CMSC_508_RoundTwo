6d4ebd4dc881ef7e3ae37411b4b3afcff46b4997
Harsh Patel <harshpatel23895@gmail.com>
2020-05-01 22:16:39 -0400

    index update

--- 508Project-PHP/index.php
+++ 508Project-PHP/index.php
@@ -5,5 +5,6 @@
 <ul>
 <li> <a href="list_employees.php"> List all Employees </a> </li>
 <li> <a href="list_products.php"> Products Available </a> </li>
+<li> <a href="add_products.php"> Add a Product </a> </li>
 </ul>
 </body>
