06b7384c3a662a7e0f596288bb2896c662f45e09
ujjwalbtra21 <ujjwalbatra21@gmail.com>
2020-05-02 14:44:23 -0400

    Added customer link

--- 508Project-PHP/index.php
+++ 508Project-PHP/index.php
@@ -5,6 +5,7 @@
 <ul>
 <li> <a href="list_employees.php"> List all Employees </a> </li>
 <li> <a href="list_products.php"> Products Available </a> </li>
+<li> <a href="add_customer.php"> Register as a Customer </a> </li>
 <li> <a href="login.php"> Employee Login </a> </li>
 </ul>
 </body>
