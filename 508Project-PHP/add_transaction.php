18dfec694ea65e7347915ab1fceeb733d02601cb
Harsh Patel <harshpatel23895@gmail.com>
2020-05-03 17:46:40 -0400

    adding add_transaction

---
+++ 508Project-PHP/add_transaction.php
@@ -0,0 +1,84 @@
+<?php
+
+require_once 'connection.php';
+
+echo "<div align = 'center'>";
+echo "<h3> Purchase a Product. </h3><br />";
+
+echo "<form method='post' action='add_transaction.php'>";
+echo "<table style='border: solid 1px black;'>";
+echo "<tbody>";
+
+echo "<tr><td>Phone Number</td>";
+echo "<td><input type='text' name='customer_phone' size='15'></td></tr>";
+
+echo "<tr><td>Payment Method</td>";
+echo "<td><select name='payment'>";
+echo "<option value='0' selected>No Payment</option>";
+echo "<option value='1'>Cash</option>";
+echo "<option value='2'>Credit Card</option>";
+echo "</select></td>";
+echo "<td><select name='type'>";
+echo "<option value='No Type' selected>No Type</option>";
+echo "<option value='VISA'>VISA</option>";
+echo "<option value='MC'>MC</option>";
+echo "<option value='AMEX'>AMEX</option>";
+echo "<option value='DIS'>DISCOVER</option>";
+echo "</select></td></tr>";
+
+echo "<tr><td>Transation Date</td>";
+echo "<td><input type='date' name='transaction_date'></td></tr>";
+
+echo "<tr><td>Product Name</td>";
+//$in_stock = 'In Stock';
+$stmt = $conn->prepare("SELECT productID, product_name FROM product WHERE in_stock='In Stock'");
+$stmt->execute();
+echo "<td><select name='product'>";
+echo "<option value='-1'>No Product</option>";
+while($row = $stmt->fetch()){
+    echo "<option value='$row[productID]'>$row[product_name]</option>";
+}
+echo "</select></td></tr>";
+
+echo "<tr><td><input type='submit' name='submit' value='DONE'></td></tr>";
+
+echo "</tbody>";
+echo "</table>";
+echo "</form>";
+echo "</div>";
+
+if(isset($_POST['submit'])){
+     
+    $stmt = $conn->prepare("INSERT INTO transaction (payment_method, date, productID, customer_phone)
+                            VALUES (:payment, :date, :product, :customer_phone)");
+    
+    if($_POST['payment'] == 1){
+        $stmt->bindValue(':payment', 'CASH');
+    }
+    if($_POST['payment'] == 2){
+        $str1 = 'CC-';
+        $str2 = $_POST['type'];
+        $str3 = $str1.$str2;
+        $stmt->bindValue(':payment', $str3, PDO::PARAM_STR);
+    }
+    
+    $stmt->bindValue(':date', $_POST['transaction_date']);
+    
+    if($_POST['product'] != -1){
+        $stmt->bindValue(':product', $_POST['product']);
+    }
+    
+    $stmt->bindValue(':customer_phone', $_POST['customer_phone']);
+    $stmt->execute();
+    
+    $product_id = $_POST['product'];
+    $stmt = $conn->prepare("UPDATE product SET in_stock='Sold' WHERE productID=$product_id");
+    //$stmt->bindValue(':product_id', $product_id);
+    $stmt->execute();
+    
+    echo "<div align = 'center'>";
+    echo "<h4>Success! Transaction added to database.<h4>";
+    echo "</div>";
+}
+
+?>
--- 508Project-PHP/customer_check.php
+++ 508Project-PHP/customer_check.php
@@ -6,7 +6,7 @@ echo "<h3> Enter Customer Phone Number. </h3><br />";
 
 echo "<form method='post' action='customer_check.php'>";
 
-echo "<label>Phone Number: </label>";
+echo "<label>Phone Number: </label><br />";
 echo "<input type='text' name='phone_number' size='12'><br />";
 echo "<input type='submit' name='submit' value='CHECK'>";
 echo "</form>";
@@ -19,10 +19,14 @@ if(isset($_POST['submit'])){
     $result = $query->fetch();
     
     if($result){
-        echo "Customer already Exits";
+        echo "<h5 style='color:green;'><b> Customer already Exits</b></h5><br />";
+        echo "<h4> Do you want to make any transaction? Click 'Make Transaction'.</h4><br />";
+        echo "<a href='add_transaction.php'> Make Transaction </a>";
     }
     else{
-        echo "<a href='buying_product.php'> Add Customer and Create Transaction. </a>";
+        echo "<h5 style='color:red;'><b> Customer does not Exits</b></h5><br />";
+        echo "<h4> Do you want to add the Customer and Transaction? Click 'Add to Records'.</h4><br />";
+        echo "<a href='buying_product.php'> Add Customer and Transaction. </a>";
     }
 }
 echo "</div>";
