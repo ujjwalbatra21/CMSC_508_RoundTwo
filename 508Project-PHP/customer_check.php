1213a5807c884d66b3ec2ed0a24a4c997ef57824
Harsh Patel <harshpatel23895@gmail.com>
2020-05-03 16:43:14 -0400

    update

--- 508Project-PHP/customer_check.php
+++ 508Project-PHP/customer_check.php
@@ -7,24 +7,23 @@ echo "<h3> Enter Customer Phone Number. </h3><br />";
 echo "<form method='post' action='buying_product.php'>";
 
 echo "<label>Phone Number: </label>";
-echo "<input type='text' name='customer_phone' size='12'>";
+echo "<input type='text' name='customer_phone' size='12'><br />";
 echo "<input type='submit' name='submit' value='CHECK'>";
 echo "</form>";
 
 if(isset($_POST['submit'])){
     
-$phone = $_POST['customer_phone'];
-$query = "SELECT * FROM customer WHERE customer_phone=$phone";
-$query->execute();
-$result = $query->fetch();
-
-if($result){
-    echo"Customer Exits";
+    $phone = $_POST['customer_phone'];
+    $query = $conn->prepare("SELECT * FROM customer WHERE customer_phone=$phone");
+    $query->execute();
+    $result = $query->fetch();
+    
+    if($result){
+        echo "Customer already Exits";
+    }
+    else{
+        echo "<a href='buying_product.php'> Add Customer and Create Transaction. </a>";
+    }
 }
-else{
-    echo "<a href='buying_product.php'> Add Customer and Create Transaction. </a>";
-}
-
 echo "</div>";
-}
 ?>
