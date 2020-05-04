8803c6125474a69b29a80b8530ca96a52994ba3e
Harsh Patel <harshpatel23895@gmail.com>
2020-05-04 00:48:49 -0400

    update

--- 508Project-PHP/delete_record.php
+++ 508Project-PHP/delete_record.php
@@ -7,13 +7,16 @@ echo "<h3> Purchase a Product. </h3><br />";
 
 echo "<form method='post' action='delete_record.php'>";
 
-echo "<h5> Please select what record you want to delete:</h5> <br />";
-echo "<label> Product </label><br>";
+echo "<h4> Please select what record you want to delete:</h4> <br />";
+
 echo "<input type='radio' name='product' value='Product'>";
-echo "<label> Transaction </label><br>";
+echo "<label> Product </label><br>";
+
 echo "<input type='radio' name='transaction' value='Transaction'>";
-echo "<label> Employee </label><br>";
+echo "<label> Transaction </label><br>";
+
 echo "<input type='radio' name='employee' value='Employee'>";
+echo "<label> Employee </label><br>";
 
 echo "<input type='submit' name='submit' value='SELECT'></td></tr>";
 
@@ -24,7 +27,33 @@ echo "</div>";
 if(isset($_POST['submit'])){
     if(isset($_POST['product']))
     {
-        echo "You have selected :".$_POST['product'];
+        echo "<form method='post' action='add_transaction.php'>";
+        echo "<table>";
+        echo "<tbody>";
+        echo "<tr><td>Select Product Name: </td>";
+        $stmt = $conn->prepare("SELECT productID, product_name, in_stock FROM product");
+        $stmt->execute();
+        echo "<td><select name='product'>";
+        echo "<option value='-1'>No Product</option>";
+        while($row = $stmt->fetch()){
+            echo "<option value='$row[productID]'>$row[product_name] $row[in_stock]</option>";
+        }
+        echo "</select></td></tr>";
+        echo "<tr><td><input type='submit' name='go_product' value='GO!'></td></tr>";
+        echo "</tbody>";
+        echo "</table>";
+        echo "</form>";
+        
+        if(isset($_POST['go_product'])){
+            $product = $_POST['product'];
+            if($product != -1){
+                $stmt = $conn->prepare("DELETE FROM product WHERE productID=$product");
+                $stmt->execute();
+            }
+            else{
+                echo "<h5 style='color:red;'>No product selected</h5>";
+            }
+        }
     }
     
     if(isset($_POST['transaction']))
