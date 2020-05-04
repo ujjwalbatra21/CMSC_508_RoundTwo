23890fe5823fa6189ec2e0359c69270a921ee669
Harsh Patel <harshpatel23895@gmail.com>
2020-05-03 20:42:49 -0400

    updated add_products

--- 508Project-PHP/add_products.php
+++ 508Project-PHP/add_products.php
@@ -7,35 +7,93 @@ echo "<form method='post' action='add_products.php'>";
 echo "<table style='border: solid 1px black;'>";
 echo "<tbody>";
     	echo "<tr><td>Product Name</td>";
-    	echo "<td><input type='text' name='productname' size='15'></td></tr>";
+    	echo "<td><input type='text' name='productname' size='25'></td></tr>";
+    	
     	echo "<tr><td>Shoe ID</td>";
-    	echo "<td><input type='text' name='shoeid' size='15'></td></tr>";
+    	echo "<td><input type='text' name='shoeid' size='25' placeholder='Required if Adding Shoes'></td></tr>";
+    	
+    	echo "<tr><td>Shoe Size</td>";
+    	echo "<td><select name='shoesize'>";
+    	echo "<option value='-1' selected>No Size</option>";
+    	echo "<option value='1.0'>1</option>";
+    	echo "<option value='1.5'>1.5</option>";
+    	echo "<option value='2.0'>2</option>";
+    	echo "<option value='2.5'>2.5</option>";
+    	echo "<option value='3.0'>3</option>";
+    	echo "<option value='3.5'>3.5</option>";
+    	echo "<option value='4.0'>4</option>";
+    	echo "<option value='4.5'>4.5</option>";
+    	echo "<option value='5.0'>5</option>";
+    	echo "<option value='5.5'>5.5</option>";
+    	echo "<option value='6.0'>6</option>";
+    	echo "<option value='6.5'>6.5</option>";
+    	echo "<option value='7.0'>7</option>";
+    	echo "<option value='7.5'>7.5</option>";
+    	echo "<option value='8.0'>8</option>";
+    	echo "<option value='8.5'>8.5</option>";
+    	echo "<option value='9.0'>9</option>";
+    	echo "<option value='9.5'>9.5</option>";
+    	echo "<option value='10.0'>10</option>";
+    	echo "<option value='10.5'>10.5</option>";
+    	echo "<option value='11.0'>11</option>";
+    	echo "<option value='11.5'>11.5</option>";
+    	echo "<option value='12.0'>12</option>";
+    	echo "<option value='12.5'>12.5</option>";
+    	echo "<option value='13.0'>13</option>";
+    	echo "</select></td></tr>";
+    	
     	echo "<tr><td>Clothes ID</td>";
-    	echo "<td><input type='text' name='clothesid' size='15'></td></tr>";
+    	echo "<td><input type='text' name='clothesid' size='25' placeholder='Required if Adding Clothes'></td></tr>";
+    	
+    	echo "<tr><td>Clothes Size</td>";
+    	echo "<td><select name='clothessize'>";
+    	echo "<option value='-1' selected>No Size</option>";
+    	echo "<option value='XS'>X-Small</option>";
+    	echo "<option value='S'>Small</option>";
+    	echo "<option value='M'>Medium</option>";
+    	echo "<option value='L'>Large</option>";
+    	echo "<option value='XL'>X-Large</option>";
+    	echo "<option value='XXL'>XX-Large</option>";
+    	echo "</select></td></tr>";
+    	
     	echo "<tr><td>Brand</td>";
     	echo "<td><input type='text' name='brand' size='15'></td></tr>";
+    	
+    	echo "<tr><td>Color</td>";
+    	echo "<td><input type='text' name='colorway' size='10'></td></tr>";
+    	
+    	echo "<tr><td>Type</td>";
+    	echo "<td><input type='text' name='type' size='15'></td></tr>";
+    	
+    	echo "<tr><td>Gender</td>";
+    	echo "<td><select name='gender'>";
+    	echo "<option value='-1' selected>No Gender</option>";
+    	echo "<option value='Mens'>Mens</option>";
+    	echo "<option value='Women'>Women</option>";
+    	echo "<option value='Unisex'>Unisex</option>";
+    	echo "<option value='Kids'>Kids</option>";
+    	echo "</select></td></tr>";
+    	
     	echo "<tr><td>Condition</td>";
     	echo "<td><select name='condition'>";
-		echo "<option value='default' selected>Select Condition</option>";
-		echo "<option value='new'>New</option>";
-  		echo "<option value='used'>Used</option>";
+		echo "<option value='-1' selected>No Condition</option>";
+		echo "<option value='New'>New</option>";
+  		echo "<option value='Used'>Used</option>";
   		echo "</select></td></tr>";
+  		
         echo "<tr><td>Price</td>";
         echo "<td><input type='text' name='price' size='10'></td></tr>";
-        echo "<tr><td>Color</td>";
-        echo "<td><input type='text' name='colorway' size='10'></td></tr>";
-        echo "<tr><td>Type</td>";
-        echo "<td><input type='text' name='type' size='15'></td></tr>";
-        echo "<tr><td>Gender</td>";
-        echo "<td><select name='gender'>";
-		echo "<option value='default' selected>Select Gender</option>";
-		echo "<option value='male'>Male</option>";
-  		echo "<option value='female'>Female</option>";
-  		echo "<option value='unisex'>Unisex</option>";
-  		echo "<option value='kids'>Kids</option>";
-  		echo "</select></td></tr>";
+        
+        echo "<tr><td>In Stock</td>";
+        echo "<td><select name='instock'>";
+        echo "<option value='-1' selected>No Selected</option>";
+        echo "<option value='Yes'>Yes</option>";
+        echo "<option value='No'>No</option>";
+        echo "</select></td></tr>";
+        
         echo "<tr><td> <br /> </td></tr>";
         echo "<tr><td><input type='submit' name='submit' value='Submit'></td></tr>";
+        
 echo "</tbody>";
 echo "</table>";
 echo "</form>";
@@ -44,25 +102,66 @@ echo "</form>";
 
 if(isset($_POST['submit'])){
     
-    $stmt = $conn->prepare("INSERT INTO product (product_name, shoeID, clothesID, brand, product_condition, price, colorway, type, gender) 
-                            VALUES (:product_name, :shoe_id, :clothes_id, :brand_name, :product_condition, :product_price, :product_color, :product_type, :product_gender)");
+    $stmt = $conn->prepare("INSERT INTO product (product_name, shoeID, shoe_size, clothesID, clothes_size, brand, colorway, type, gender, product_condition, price, in_stock) 
+                            VALUES (:product_name, :shoe_id, :shoe_size, :clothes_id, :clothes_size, :brand_name, :product_color, :product_type, :product_gender, :product_condition, :product_price, :in_stock)");
     
     $stmt->bindValue(':product_name', $_POST['productname']);
-    $stmt->bindValue(':shoe_id', $_POST['shoeid']);
-    $stmt->bindValue(':clothes_id', $_POST['clothesid']);
-    $stmt->bindValue(':brand_name', $_POST['brand']);
-    $stmt->bindValue(':product_condition', $_POST['condition']);
-    $stmt->bindValue(':product_price', $_POST['price']);
-    $stmt->bindValue(':product_color', $_POST['colorway']);
-    $stmt->bindValue(':product_type', $_POST['type']);
-    $stmt->bindValue(':product_gender', $_POST['gender']);
     
     if($_POST['shoeid'] == ' '){
-        $stmt->bindValue(':shoe_id', NULL, PDO::PARAM_STR);    
+        $stmt->bindValue(':shoe_id', NULL, PDO::PARAM_STR);
     }
+    else{
+        $stmt->bindValue(':shoe_id', $_POST['shoeid']);
+    }
+    
+    if($_POST['shoesize'] == -1){
+        $stmt->bindValue(':shoe_size', NULL, PDO::PARAM_STR);
+    }
+    else{
+        $stmt->bindValue(':shoe_size', $_POST['shoesize']);
+    }
+    
     if($_POST['clothesid'] == ' '){
         $stmt->bindValue(':clothes_id', NULL, PDO::PARAM_STR);
     }
+    else{
+        $stmt->bindValue(':clothes_id', $_POST['clothesid']);
+    }
+    
+    if($_POST['clothessize'] == -1){
+        $stmt->bindValue(':clothes_size', NULL, PDO::PARAM_STR);
+    }
+    else{
+        $stmt->bindValue(':clothes_size', $_POST['clothessize']);
+    }
+    
+    $stmt->bindValue(':brand_name', $_POST['brand']);
+    $stmt->bindValue(':product_color', $_POST['colorway']);
+    $stmt->bindValue(':product_type', $_POST['type']);
+    
+    if($_POST['gender'] == -1){
+        $stmt->bindValue(':product_gender', NULL, PDO::PARAM_STR);
+    }
+    else{
+        $stmt->bindValue(':product_gender', $_POST['gender']);
+    }
+    
+    if($_POST['condition'] == -1){
+        $stmt->bindValue(':product_condition', NULL, PDO::PARAM_STR);
+    }
+    else{
+        $stmt->bindValue(':product_condition', $_POST['condition']);
+    }
+    
+    $stmt->bindValue(':product_price', $_POST['price']);
+    
+    if($_POST['instock'] == -1){
+        $stmt->bindValue(':in_stock', NULL, PDO::PARAM_STR);
+    }
+    else{
+        $stmt->bindValue(':in_stock', $_POST['instock']);
+    }
+    
     
     $stmt->execute();
     
@@ -71,3 +170,11 @@ if(isset($_POST['submit'])){
 }
 
 ?>
+
+<html>
+<body>
+<div align = "center">
+	<a href="index.php"> Go Back to Main Page </a>
+</div>
+</body>
+</html>
--- 508Project-PHP/index.php
+++ 508Project-PHP/index.php
@@ -1,12 +1,19 @@
 <html>
 <head> <title> Round-Two Index </title> </head>
 <body>
-<h2> Round Two Database </h2> <br /> <br />
-<ul>
-<li> <a href="list_employees.php"> List all Employees </a> </li>
-<li> <a href="list_products.php"> Products Available </a> </li>
-<li> <a href="add_customer.php"> Register as a Customer </a> </li>
-<li> <a href="login.php"> Employee Login </a> </li>
-<li> <a href="customer_check.php"> Purchase Products </a> </li>
-</ul>
+<div align ="center">
+<br /> <br />
+<h1> Round Two Database </h1> <br /> <br />
+<hr/>
+
+<ol>
+    <li> <a href="list_employees.php"> List all Employees </a> </li>
+    <li> <a href="list_products.php"> Products Available </a> </li>
+    <li> <a href="add_products.php"> Add a Product </a> </li>
+    <li> <a href="customer_check.php"> Purchase Products </a> </li>
+    
+    <!-- <li> <a href="login.php"> Employee Login </a> </li> -->
+</ol>
+
+</div>
 </body>
