6163c32672f401f9afd4c6aabb71fe9c02e5aab6
Harsh Patel <harshpatel23895@gmail.com>
2020-05-03 21:26:06 -0400

    update index

--- 508Project-PHP/index.php
+++ 508Project-PHP/index.php
@@ -1,12 +1,20 @@
 <html>
-<head> <title> Round-Two Index </title> </head>
+<head>
+<title> Round-Two Index </title>
+<style>
+.center {
+  text-align: center;
+  list-style-position: inside;
+}
+</style>
+</head>
 <body>
 <div align ="center">
 <br /> <br />
 <h1> Round Two Database </h1> <br /> <br />
 <hr/>
 
-<ol>
+<ol class="center">
     <li> <a href="list_employees.php"> List all Employees </a> </li>
     <li> <a href="list_products.php"> Products Available </a> </li>
     <li> <a href="add_products.php"> Add a Product </a> </li>
